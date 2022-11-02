4ebcb0c956c37f3f12849dfcd148a56ebe809ec2
tatsuki1107 <tatsuki11070522@icloud.com>
2022-11-02 19:16:13 +0900

    new Matrix Factorization

--- util/data_loader.py
+++ util/data_loader.py
@@ -31,3 +31,77 @@ class DataLoader:
         ])
         
         return dataset
+    
+    def train_rating() -> list:
+        
+        dataset = [
+    { "user_id": 1, "item_id": 2, "rating": 4},
+    { "user_id": 1, "item_id": 3, "rating": 3},
+    { "user_id": 1, "item_id": 4, "rating": 1},
+    { "user_id": 1, "item_id": 5, "rating": 2},
+    { "user_id": 1, "item_id": 7, "rating": 4},
+    { "user_id": 2, "item_id": 1, "rating": 5},
+    { "user_id": 2, "item_id": 2, "rating": 5},
+    { "user_id": 2, "item_id": 3, "rating": 4},
+    { "user_id": 2, "item_id": 5, "rating": 3},
+    { "user_id": 2, "item_id": 6, "rating": 3},
+    { "user_id": 2, "item_id": 7, "rating": 5},
+    { "user_id": 2, "item_id": 8, "rating": 4},
+    { "user_id": 3, "item_id": 1, "rating": 4},
+    { "user_id": 3, "item_id": 3, "rating": 5},
+    { "user_id": 3, "item_id": 4, "rating": 3},
+    { "user_id": 3, "item_id": 5, "rating": 2},
+    { "user_id": 3, "item_id": 8, "rating": 3},
+    { "user_id": 4, "item_id": 2, "rating": 3},
+    { "user_id": 4, "item_id": 4, "rating": 2},
+    { "user_id": 4, "item_id": 5, "rating": 1},
+    { "user_id": 4, "item_id": 6, "rating": 1},
+    { "user_id": 4, "item_id": 7, "rating": 3},
+    { "user_id": 5, "item_id": 1, "rating": 2},
+    { "user_id": 5, "item_id": 2, "rating": 1},
+    { "user_id": 5, "item_id": 3, "rating": 2},
+    { "user_id": 5, "item_id": 4, "rating": 4},
+    { "user_id": 5, "item_id": 6, "rating": 3},
+    { "user_id": 5, "item_id": 7, "rating": 2},
+]
+        return dataset
+    
+    def test_rating() -> list:
+        
+        # 内容ブーステッド協調フィルタリングの予測値を仮のテストデータとする。
+        dataset = [
+            { "user_id": 1, "item_id": 1, "rating": 3},
+            { "user_id": 1, "item_id": 6, "rating": 2},
+            { "user_id": 1, "item_id": 8, "rating": 3},
+            { "user_id": 1, "item_id": 9, "rating": 4},
+            { "user_id": 2, "item_id": 4, "rating": 3},
+            { "user_id": 2, "item_id": 9, "rating": 4},
+            { "user_id": 3, "item_id": 2, "rating": 4},
+            { "user_id": 3, "item_id": 6, "rating": 2},
+            { "user_id": 3, "item_id": 7, "rating": 3},
+            { "user_id": 3, "item_id": 9, "rating": 3},
+            { "user_id": 4, "item_id": 1, "rating": 3},
+            { "user_id": 4, "item_id": 3, "rating": 3},
+            { "user_id": 4, "item_id": 8, "rating": 3},
+            { "user_id": 4, "item_id": 9, "rating": 3},
+            { "user_id": 5, "item_id": 5, "rating": 5},
+            { "user_id": 5, "item_id": 8, "rating": 1},
+            { "user_id": 5, "item_id": 9, "rating": 1},
+        ]
+        
+        return dataset
+    
+    def item() -> list:
+        
+        dataset = [
+    {"item_id": 1, "type": "赤身"},
+    {"item_id": 2, "type": "赤身"},
+    {"item_id": 3, "type": "赤身"},
+    {"item_id": 4, "type": "白身"},
+    {"item_id": 5, "type": "白身"},
+    {"item_id": 6, "type": "白身"},
+    {"item_id": 7, "type": "光物"},
+    {"item_id": 8, "type": "光物"},
+    {"item_id": 9, "type": "光物"},
+]
+        return dataset
